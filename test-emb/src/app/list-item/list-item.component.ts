b576665df499e7c856bdbda88580d637c5612b55
Anton <hramickih@yandex.ru>
2022-02-28 13:09:26 +0000

    feat: add favicon and title

--- test-emb/src/app/list-item/list-item.component.ts
+++ test-emb/src/app/list-item/list-item.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListItemComponent implements OnInit {
 
-  titleItem = '';
+  titleItem = 'Blick-Durgan';
   linkItem = '';
-  
+
 
   constructor() { }
 
